7182366f0763ca7748807a3c7541be25425209b9
muhtartatar <132200636+muhtartatar@users.noreply.github.com>
2023-04-30 18:17:28 +0100

    Changes

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-a = 2
-b = 4
-max_num = a if a > b else b
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.rout("/")
+def hello_world():
+    return "<h3>Hello World!!!<h3>"
+
+
+#if __name__ == "__maim__":
+    #app.run(host='0.0.0.0', port=8000, debug=True)
